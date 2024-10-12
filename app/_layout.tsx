80f9a85cec194a1288c606c5a5b453668e461d6d
saad-official <tommmyy890@gmail.com>
2024-10-12 12:29:16 +0500

    added tabs bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,33 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
-
+import { Stack, Tabs } from "expo-router";
+import { Entypo } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 const Layout = () => {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#29b365" }}>
+      <Tabs.Screen
         name="index"
         options={{
           headerTitle: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
         }}
       />
-    </Stack>
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
